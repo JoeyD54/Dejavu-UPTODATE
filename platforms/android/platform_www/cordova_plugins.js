bb7e67dcc19dabd32850444cf403fbf1aba5269f
Joey Domino <playdude224@hotmail.com>
2019-07-29 19:00:12 -0400

    init
    init

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,153 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-device.device",
+      "file": "plugins/cordova-plugin-device/www/device.js",
+      "pluginId": "cordova-plugin-device",
+      "clobbers": [
+        "device"
+      ]
+    },
+    {
+      "id": "cordova-plugin-permission.Permission",
+      "file": "plugins/cordova-plugin-permission/www/index.js",
+      "pluginId": "cordova-plugin-permission",
+      "clobbers": [
+        "window.plugins.Permission"
+      ]
+    },
+    {
+      "id": "cordova-plugin-permission.tests",
+      "file": "plugins/cordova-plugin-permission/tests/index.spec.js",
+      "pluginId": "cordova-plugin-permission"
+    },
+    {
+      "id": "cordova-plugin-android-permissions.Permissions",
+      "file": "plugins/cordova-plugin-android-permissions/www/permissions.js",
+      "pluginId": "cordova-plugin-android-permissions",
+      "clobbers": [
+        "cordova.plugins.permissions"
+      ]
+    },
+    {
+      "id": "cordova-plugin-geolocation.geolocation",
+      "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+      "pluginId": "cordova-plugin-geolocation",
+      "clobbers": [
+        "navigator.geolocation"
+      ]
+    },
+    {
+      "id": "cordova-plugin-geolocation.PositionError",
+      "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+      "pluginId": "cordova-plugin-geolocation",
+      "runs": true
+    },
+    {
+      "id": "cordova.plugins.diagnostic.Diagnostic",
+      "file": "plugins/cordova.plugins.diagnostic/www/android/diagnostic.js",
+      "pluginId": "cordova.plugins.diagnostic",
+      "merges": [
+        "cordova.plugins.diagnostic"
+      ]
+    },
+    {
+      "id": "cordova.plugins.diagnostic.Diagnostic_Location",
+      "file": "plugins/cordova.plugins.diagnostic/www/android/diagnostic.location.js",
+      "pluginId": "cordova.plugins.diagnostic",
+      "merges": [
+        "cordova.plugins.diagnostic.location"
+      ]
+    },
+    {
+      "id": "cordova.plugins.diagnostic.Diagnostic_Bluetooth",
+      "file": "plugins/cordova.plugins.diagnostic/www/android/diagnostic.bluetooth.js",
+      "pluginId": "cordova.plugins.diagnostic",
+      "merges": [
+        "cordova.plugins.diagnostic.bluetooth"
+      ]
+    },
+    {
+      "id": "cordova.plugins.diagnostic.Diagnostic_Wifi",
+      "file": "plugins/cordova.plugins.diagnostic/www/android/diagnostic.wifi.js",
+      "pluginId": "cordova.plugins.diagnostic",
+      "merges": [
+        "cordova.plugins.diagnostic.wifi"
+      ]
+    },
+    {
+      "id": "cordova.plugins.diagnostic.Diagnostic_Camera",
+      "file": "plugins/cordova.plugins.diagnostic/www/android/diagnostic.camera.js",
+      "pluginId": "cordova.plugins.diagnostic",
+      "merges": [
+        "cordova.plugins.diagnostic.camera"
+      ]
+    },
+    {
+      "id": "cordova.plugins.diagnostic.Diagnostic_Notifications",
+      "file": "plugins/cordova.plugins.diagnostic/www/android/diagnostic.notifications.js",
+      "pluginId": "cordova.plugins.diagnostic",
+      "merges": [
+        "cordova.plugins.diagnostic.notifications"
+      ]
+    },
+    {
+      "id": "cordova.plugins.diagnostic.Diagnostic_Microphone",
+      "file": "plugins/cordova.plugins.diagnostic/www/android/diagnostic.microphone.js",
+      "pluginId": "cordova.plugins.diagnostic",
+      "merges": [
+        "cordova.plugins.diagnostic.microphone"
+      ]
+    },
+    {
+      "id": "cordova.plugins.diagnostic.Diagnostic_Contacts",
+      "file": "plugins/cordova.plugins.diagnostic/www/android/diagnostic.contacts.js",
+      "pluginId": "cordova.plugins.diagnostic",
+      "merges": [
+        "cordova.plugins.diagnostic.contacts"
+      ]
+    },
+    {
+      "id": "cordova.plugins.diagnostic.Diagnostic_Calendar",
+      "file": "plugins/cordova.plugins.diagnostic/www/android/diagnostic.calendar.js",
+      "pluginId": "cordova.plugins.diagnostic",
+      "merges": [
+        "cordova.plugins.diagnostic.calendar"
+      ]
+    },
+    {
+      "id": "cordova.plugins.diagnostic.Diagnostic_NFC",
+      "file": "plugins/cordova.plugins.diagnostic/www/android/diagnostic.nfc.js",
+      "pluginId": "cordova.plugins.diagnostic",
+      "merges": [
+        "cordova.plugins.diagnostic.nfc"
+      ]
+    },
+    {
+      "id": "cordova.plugins.diagnostic.Diagnostic_External_Storage",
+      "file": "plugins/cordova.plugins.diagnostic/www/android/diagnostic.external_storage.js",
+      "pluginId": "cordova.plugins.diagnostic",
+      "merges": [
+        "cordova.plugins.diagnostic.external_storage"
+      ]
+    },
+    {
+      "id": "cordova-plugin-request-location-accuracy.RequestLocationAccuracy",
+      "file": "plugins/cordova-plugin-request-location-accuracy/www/android/RequestLocationAccuracy.js",
+      "pluginId": "cordova-plugin-request-location-accuracy",
+      "clobbers": [
+        "cordova.plugins.locationAccuracy"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-vibration": "3.1.1",
+    "cordova-plugin-device": "2.0.3",
+    "cordova-plugin-permission": "0.1.0",
+    "cordova-plugin-android-permissions": "1.0.0",
+    "cordova-plugin-geolocation": "4.0.2",
+    "cordova.plugins.diagnostic": "5.0.0",
+    "cordova-plugin-request-location-accuracy": "2.3.0"
+  };
+});
---
+++ www/js/eventHandler.js
@@ -0,0 +1,202 @@
+/**************************************************
+* This javascript file handles all event handlers *
+* for the program                                 * 
+**************************************************/
+
+/*
+$("#insertButton").on("touched", function(){
+    alert('jquery works');
+});
+*/
+
+function showModal(){
+    document.getElementById('toDoModal').style.display='block';
+}
+
+function closeModal(){
+    document.getElementById('toDoModal').style.display='none';
+}
+
+function onBackKeyDown(event){
+    if(document.getElementById('toDoModal').style.display="block"){
+            document.getElementById('toDoModal').style.display="none";          
+    }
+}
+
+function loadXMLTable(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            displayXMLTable(this);
+        }
+    };
+    xmlhttp.open("GET", "xml/toDoList.xml", true);
+    xmlhttp.send();
+    /*
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "toDoList.xml", true);
+    xhttp.send();
+    displayXMLTable(xhttp);
+    */
+    /*
+    var xhttp = new XMLHttpRequest();
+    document.getElementById('printHere').innerHTML = xhttp;
+    
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            displayXMLTable(this);
+        }
+    };
+    xhttp.open("GET", "xml/toDoList.xml", true);
+    xhttp.send();
+    */
+}
+
+function displayXMLTable(xml){
+    var i;
+    var xmlDoc = xml.responseXML;
+    var table="<tr><th>Item</th><th>Destination</th></tr>";
+    var x = xmlDoc.getElementsByTagName("todo");
+    for (i = 0; i <x.length; i++) { 
+        table += "<tr><td>" +
+        x[i].getElementsByTagName("item")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("dest")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        "<span id=i onclick='removeXMLDocItem()' class='close'>&times;</span>" +
+        "</td></tr>";
+      
+    }
+    document.getElementById("printTable").innerHTML = table;
+    /*
+    document.getElementById('printHere').innerHTML = xml;
+    var x, i, list, item, dest, xmlDoc;
+    xmlDoc = xml.responseXML;
+    item = "";
+    dest = "";
+    x = xmlDoc.getElementsByTagName('todo');
+    var y = x[0].getElementsByTagName('item')[0].childNodes[0].nodeValue;
+    document.getElementById('printHere').innerHTML = y;
+    for(i = 0; i < x.length; i++){
+
+        item = x[i].getElementsByTagName('item')[0].childNodes[0].nodeValue;
+        dest = x[i].getElementsByTagName('dest')[0].childNodes[0].nodeValue;
+
+        /*
+        list += "<li class = 'w3-bar'> <span id = 'removeItem'" +
+            "class = 'w3-bar-item w3-button w3-white w3-large w3-right>X</span>" +
+            "<div class='w3-bar-item'> " + 
+                "<span class='w3-large'>" x[i].getElementsByTagName('item')[0].childNodes[0].nodeValue; "</span>" +
+                "<"
+            "</div";            
+    }
+   document.getElementById('printHere').innerHTML = item;
+*/
+}
+
+
+function callGoogle(){
+    //https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=tires&inputtype=textquery&fields=formatted_address,name,open_now,rating&locationbias=circle:2000@42.3330,83.0465&key=AIzaSyD1BD2SIhcmvk7SmV1NGBrgaEQOLqjx4fI;
+    
+    let options = {maximumAge: 1000 * 60 * 60,
+                   timeout: 20000, 
+                   enableHighAccuracy: true};
+
+    //if location is turned off, ask to turn it on, else run the call
+    cordova.plugins.diagnostic.isGpsLocationEnabled(function(enabled){
+        //GPS is on, find location
+        if(enabled == "enabled"){
+
+        } else {
+            requestGPS();
+        }
+    });
+    navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+}
+
+
+function onSuccess(position) {
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+
+    document.getElementById('printApiCall').innerHTML = "lat: " + lat + "\n long: " + long;
+    
+    //Call up google and get an item based off of phone's current position at time oh hitting button.
+    
+    var HTTPREQ = "https://maps.googleapis.com/maps/api/geocode/xml?latlng=" + lat + "," + long +"&key=AIzaSyD1BD2SIhcmvk7SmV1NGBrgaEQOLqjx4fI"; 
+
+    var URLREQ = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=tires&inputtype=textquery&fields=formatted_address,name,rating&locationbias=circle:2000@42.3330,83.0465&key=AIzaSyD1BD2SIhcmvk7SmV1NGBrgaEQOLqjx4fI";
+
+    var LATLONG = lat + "," + long;
+    var KEY = "AIzaSyD1BD2SIhcmvk7SmV1NGBrgaEQOLqjx4fI";
+    $.ajax({
+        //url: "https://maps.googleapis.com/maps/api/place/findplacefromtext/json",
+        //url: URLREQ,
+        url: "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=tires&inputtype=textquery&fields=formatted_address,name,rating&locationbias=circle:2000@42.3330,83.0465&key=AIzaSyD1BD2SIhcmvk7SmV1NGBrgaEQOLqjx4fI",
+        type: "GET",
+        success: function(data){
+            alert('data');
+        }
+    });
+    //.done(function(data){
+    //    alert('data');
+    //});
+    //.error(function(error){
+    //    alert("failed call");
+    //});
+    /*
+    $.getJSON({
+        url: "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=tires&inputtype=textquery&fields=formatted_address,name,open_now,rating&locationbias=circle:2000@42.3330,83.0465&key=AIzaSyD1BD2SIhcmvk7SmV1NGBrgaEQOLqjx4fI",
+        type: "GET"
+    })
+    .done(function(data){
+        alert(data); //print the data out
+    });
+    */
+    /*
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            getGoogleXML(this);
+        }
+    };
+    xmlhttp.open("GET", HTTPREQ, true);
+    xmlhttp.send();
+*/
+    //document.getElementById('printApiCall').innerHTML = returnJSON;
+}
+
+function getGoogleXML(xml){
+    var i;
+    var xmlDoc = xml.responseXML;
+    var x = xmlDoc.getElementsByTagName("result")[1];
+    var y = x[1].getElementsByTagName("address_component")[0].childNodes[0].nodeValue
+    document.getElementById('printApiCall').innerHTML = x;
+
+}
+
+function onError(error){
+    alert('code: '  + error.code    + '\n' +
+        'message: ' + error.message + '\n');
+}
+
+function requestGPS(){
+    cordova.plugins.locationAccuracy.canRequest(function(canRequest){
+        if(canRequest){
+            cordova.plugins.locationAccuracy.request(function(success){
+                console.log("Successfully requested accuracy: " + success.message);
+            }, function(error){
+                console.error("Accuracy request failed: error code: " + error.code + "; error message=" + error.message);
+                if(error.code !== cordova.plugins.locationAccuracy.ERROR_USER_DISAGREED){
+                    if(window.confirm("Failed to automatically set Location mode to 'High Accuracy'."+
+                        " Would you like to switch to the Location Settings page and do it manually?")){
+                            cordova.plugins.diagnostic.switchToLocationSettings();
+                    }
+                }
+            }, cordova.plugins.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY);
+        } else {
+            //If you can't make a location request, get permission and ask again.
+            app.geoPerm;
+        }
+    });
+}
